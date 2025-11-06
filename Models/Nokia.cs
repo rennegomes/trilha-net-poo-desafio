5b16705e9d74696dccdc2820c8151e440ce416ab
rennegomes <renne.lace082@gmail.com>
2025-11-06 16:52:56 -0300

    todo Nokia concluido

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string mei, int memoria) : base(numero, modelo, mei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"{nomeApp} intalado no Nokia");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("81999999999", "N1", "222222222", 8);
+Console.WriteLine("-Celular Nokia-");
+nokia.Ligar();
+nokia.InstalarAplicativo("League of legends");
